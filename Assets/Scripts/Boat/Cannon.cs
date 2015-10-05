86e5682fa5c07f1be971ce28e44588c5acd3abd0
Collix <15986@ma-web.nl>
2015-10-05 13:41:37 +0200

    Implementing cannon shooting
    Implemented and adjusted cannon shooting system.

--- Assets/Scripts/Boat/Cannon.cs
+++ Assets/Scripts/Boat/Cannon.cs
@@ -11,6 +11,11 @@ public class Cannon : MonoBehaviour
     private float _maxAngle;
     [SerializeField]
     private float _minAngle;
+    [SerializeField]
+    private GameObject _cannonBall;
+    [SerializeField]
+    private float _force;
+    private ParticleSystem Particles;
 
     #endregion
 
@@ -18,11 +23,15 @@ public class Cannon : MonoBehaviour
 
     void Start()
     {
+        Particles = GetComponentInChildren<ParticleSystem>();
     }
 
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Shoot();
+        }
     }
 
     public void ApplyRotation(float angleNormal)
@@ -33,6 +42,21 @@ public class Cannon : MonoBehaviour
         // Apply angle
         float desiredRotation = _minAngle + (_maxAngle - _minAngle) * angleNormal;
         _barrel.transform.localRotation = Quaternion.Euler(desiredRotation, 0, 0);
+    }
+
+    public void Shoot()
+    {
+        if(_barrel == null)
+        {
+            Destroy(this);
+            return;
+        }
+
+        GameObject newProjectile = (GameObject)Instantiate(_cannonBall, _barrel.transform.position + _barrel.transform.forward * 0.7f * _barrel.transform.lossyScale.z, _barrel.transform.rotation);
+        newProjectile.GetComponent<Rigidbody>().velocity = transform.parent.GetComponent<Rigidbody>().velocity;
+        newProjectile.GetComponent<Rigidbody>().AddForce(_barrel.transform.forward * _force);
+
+        Particles.Play();
     }
 
     #endregion
